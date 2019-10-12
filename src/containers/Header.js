90f50f777bf78a36ba12e166dceaad45cdc70126
BassV <khorotyanvahagn@gmail.com>
2019-10-12 19:47:18 +0400

    A lot of issues fixed

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,145 +1,162 @@
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Search from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import styles from './Header.module.css';
-import { Checkbox, ListItemText } from '@material-ui/core';
-import StatusOptions from '../entities/StatusOptions';
-import FilterOptions from '../entities/FilterOptions';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Search from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import styles from "./Header.module.css";
+import { Checkbox, ListItemText } from "@material-ui/core";
+import StatusOptions from "../entities/StatusOptions";
+import FilterOptions from "../entities/FilterOptions";
 
-const allStatusOptions = [
-    StatusOptions.Open,
-    StatusOptions.Closed
-];
+const allStatusOptions = [StatusOptions.Open, StatusOptions.Closed];
 
 class Header extends Component {
-
-    state = {
-        statusSelector: {
-            status: [StatusOptions.Open, StatusOptions.Closed],
-            open: false
-        },
-        filterSelector: {
-            filter: FilterOptions.None,
-            open: false
-        }
+  state = {
+    statusSelector: {
+      status: [StatusOptions.Open, StatusOptions.Closed],
+      open: false
+    },
+    filterSelector: {
+      filter: FilterOptions.None,
+      open: false
     }
+  };
 
-    handleStatusChange = event => {
-        let statusSelector = {...this.state.statusSelector};
-        statusSelector.status = event.target.value;
-        statusSelector.open = false;
+  handleStatusChange = event => {
+    let statusSelector = { ...this.state.statusSelector };
+    statusSelector.status = event.target.value;
+    statusSelector.open = false;
 
-        this.props.onStatusChange(statusSelector.status);
-        this.setState({ statusSelector });
-    }
+    this.props.onStatusChange(statusSelector.status);
+    this.setState({ statusSelector });
+  };
 
-    handleStatusOpen = () => {
-        let statusSelector = {...this.state.statusSelector};
-        statusSelector.open = true;
+  handleStatusOpen = () => {
+    let statusSelector = { ...this.state.statusSelector };
+    statusSelector.open = true;
 
-        this.setState({ statusSelector })
-    }
+    this.setState({ statusSelector });
+  };
 
-    handleStatusClose = () => {
-        let statusSelector = {...this.state.statusSelector};
-        statusSelector.open = false;
+  handleStatusClose = () => {
+    let statusSelector = { ...this.state.statusSelector };
+    statusSelector.open = false;
 
-        this.setState({ statusSelector })
-    }
+    this.setState({ statusSelector });
+  };
 
-    handleFilterChange = event => {
-        let filterSelector = {...this.state.filterSelector};
-        const newFilter = event.target.value;
-        filterSelector.filter = newFilter;
-        filterSelector.open = false;
+  handleFilterChange = event => {
+    let filterSelector = { ...this.state.filterSelector };
+    const newFilter = event.target.value;
+    filterSelector.filter = newFilter;
+    filterSelector.open = false;
 
-        this.props.onFilterChange(newFilter);
+    this.props.onFilterChange(newFilter);
 
-        this.setState({ filterSelector });
-    }
+    this.setState({ filterSelector });
+  };
 
-    handleFilterOpen = () => {
-        let filterSelector = {...this.state.filterSelector};
-        filterSelector.open = true;
+  handleFilterOpen = () => {
+    let filterSelector = { ...this.state.filterSelector };
+    filterSelector.open = true;
 
-        this.setState({ filterSelector })
-    }
+    this.setState({ filterSelector });
+  };
 
-    handleFilterClose = () => {
-        let filterSelector = {...this.state.filterSelector};
-        filterSelector.open = false;
+  handleFilterClose = () => {
+    let filterSelector = { ...this.state.filterSelector };
+    filterSelector.open = false;
 
-        this.setState({ filterSelector })
-    }
+    this.setState({ filterSelector });
+  };
 
-    handleSearchTextChange = event => {
-        this.props.onSearchTextChange(event.target.value);
-    }
+  handleSearchTextChange = event => {
+    this.props.onSearchTextChange(event.target.value);
+  };
 
-    render() {
-        return (
-            <AppBar position="relative">
-                <Toolbar>
-                    <Typography className={styles.pageName} variant="h6" color="inherit" noWrap>
-                        Boomeru
-                    </Typography>
-                    <Paper className={styles.searchRoot} elevation={1}>
-                        <InputBase 
-                            className={styles.searchInput} 
-                            placeholder="Search..."
-                            onChange={this.handleSearchTextChange}/>
-                        <IconButton className={styles.searchButton} aria-label="Search">
-                            <Search/>
-                        </IconButton>
-                    </Paper>
-                    <FormControl className={styles.filterSelector}>
-                        <InputLabel style={{color: "#ffffff"}}>Filter</InputLabel>
-                        <Select 
-                            style={{color: "#ffffff"}}
-                            className={styles.selectorWidth} 
-                            open={this.state.filterSelector.open}
-                            onOpen={this.handleFilterOpen}
-                            onClose={this.handleFilterClose}
-                            value={this.state.filterSelector.filter}
-                            onChange={this.handleFilterChange}>
-                            <MenuItem value={FilterOptions.None}>{FilterOptions.None}</MenuItem>
-                            <MenuItem value={FilterOptions.Recurring}>{FilterOptions.Recurring}</MenuItem>
-                            <MenuItem value={FilterOptions.Archived}>{FilterOptions.Archived}</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <FormControl className={styles.statusSelector}>
-                        <InputLabel style={{color: "#ffffff"}}>Status</InputLabel>
-                        <Select  
-                            multiple
-                            style={{color: "#ffffff"}}
-                            className={styles.selectorWidth} 
-                            open={this.state.statusSelector.open}
-                            onOpen={this.handleStatusOpen}
-                            onClose={this.handleStatusClose}
-                            value={this.state.statusSelector.status}
-                            renderValue={selected => selected.join(', ')}
-                            onChange={this.handleStatusChange}>
-                            {allStatusOptions.map(statusOption => (
-                                <MenuItem key={statusOption} value={statusOption}>
-                                    <Checkbox checked={this.state.statusSelector.status.indexOf(statusOption) > -1}/>
-                                    <ListItemText primary={statusOption}/>
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Toolbar>
-            </AppBar>
-        );
-    }
+  render() {
+    return (
+      <AppBar position="relative">
+        <Toolbar className={styles.headerWrapper}>
+          <Typography
+            className={styles.pageName}
+            variant="h6"
+            color="inherit"
+            noWrap
+          >
+            Boomeru
+          </Typography>
+          <div className={styles.searchWrapper}>
+            <Paper className={styles.searchRoot} elevation={1}>
+              <InputBase
+                className={styles.searchInput}
+                placeholder="Search..."
+                onChange={this.handleSearchTextChange}
+              />
+              <IconButton className={styles.searchButton} aria-label="Search">
+                <Search fontSize="small" />
+              </IconButton>
+            </Paper>
+            <FormControl className={styles.filterSelector}>
+              <InputLabel style={{ color: "#ffffff" }}>Filter</InputLabel>
+              <Select
+                style={{ color: "#ffffff" }}
+                className={styles.selectorWidth}
+                open={this.state.filterSelector.open}
+                onOpen={this.handleFilterOpen}
+                onClose={this.handleFilterClose}
+                value={this.state.filterSelector.filter}
+                onChange={this.handleFilterChange}
+              >
+                <MenuItem value={FilterOptions.None}>
+                  {FilterOptions.None}
+                </MenuItem>
+                <MenuItem value={FilterOptions.Recurring}>
+                  {FilterOptions.Recurring}
+                </MenuItem>
+                <MenuItem value={FilterOptions.Archived}>
+                  {FilterOptions.Archived}
+                </MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl className={styles.statusSelector}>
+              <InputLabel style={{ color: "#ffffff" }}>Status</InputLabel>
+              <Select
+                multiple
+                style={{ color: "#ffffff" }}
+                className={styles.selectorWidth}
+                open={this.state.statusSelector.open}
+                onOpen={this.handleStatusOpen}
+                onClose={this.handleStatusClose}
+                value={this.state.statusSelector.status}
+                renderValue={selected => selected.join(", ")}
+                onChange={this.handleStatusChange}
+              >
+                {allStatusOptions.map(statusOption => (
+                  <MenuItem key={statusOption} value={statusOption}>
+                    <Checkbox
+                      checked={
+                        this.state.statusSelector.status.indexOf(statusOption) >
+                        -1
+                      }
+                    />
+                    <ListItemText primary={statusOption} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
 export default Header;
